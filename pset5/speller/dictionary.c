949b65ab02276e395d1b90a3aacc2cd9487cfeb2
QZhang-Ing <qingzhang.kit@outlook.com>
2020-06-19 18:05:49 +0000

    started implementing load function

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -1,7 +1,7 @@
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
-
+#include <string.h>
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -13,10 +13,10 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = 26;
 
 // Hash table
-node *table[N];
+node *table[N] = malloc(N*sizeof(node));
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
@@ -35,7 +35,47 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // TODO
+    // Open dictionary
+    FILE *file = fopen(dictionary, "r");
+    // Check if dictionary founded and readable
+    if (file == NULL)
+    {
+        printf("Could not open %s.\n", dictionary);
+        unload();
+        return false;
+    }
+    // Prepare to load dictionary from inout file to hash table
+    int index = 0, words = 0;
+    char dicItem[LENGTH + 1];
+
+    // get each word by characters
+    for (int c = fgetc(file); c != EOF; c = fgetc(file))
+    {
+        // get every characters untill '\n' founded
+        if (c != '\n')
+        {
+            // Append character to word
+            dicItem[index] = c;
+            index++;
+        }
+        // When a whole word founded
+        else
+        {
+            // Terminate current word to a string
+            dicItem[index] = '\0';
+
+            // Update counter
+            words++;
+            // check first character of each whole word to match the hash table
+
+
+            // Prepare for next word
+            index = 0;
+        }
+    }
+
+
+
     return false;
 }
 
@@ -51,4 +91,24 @@ bool unload(void)
 {
     // TODO
     return false;
+}
+
+// record new word from dictionary into hash table
+void newItem(char *dicItem)
+{
+    // Creates new node for words from dictionary
+    node *n = malloc(sizefo(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    int i = 0;
+    while (dicItem[i] != '\0')
+    {
+        n->word[i] = dicItem[i];
+        i++;
+    }
+    // End word with \0
+    n->word[i] = '\0';
+    n->next = NULL;
 }
