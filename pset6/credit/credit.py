08e333125c1a7dbc0151700cb1b8f7153255c7a0
QZhang-Ing <qingzhang.kit@outlook.com>
2020-06-23 06:58:05 +0000

    initial commit pset6

--- pset6/credit/credit.py
+++ pset6/credit/credit.py
@@ -1,25 +1,44 @@
-from cs50 import get_int, get_string
+from cs50 import get_int
+import math
 
 number = get_int("Number: ")
-cardNum = []
-# split number into digits stored in list cardNum
-for i in str(number):
-    cardNum.append(int(i))
-count = len(cardNum)
-# or [int(i) for i in str(number)]
+count_digits = 0
 ans = 0
-for num in cardNum[1::2]:
-    ans += (num*2)%10 + (num*2)//10
-for num in cardNum[::2]:
-    ans += num
+
+count_digits = int(math.log10(number)) + 1
+
+# initialize list to store each digits
+digits = []
+for digit in range (count_digits):
+    digits.append(number%10)
+    # integer division
+    number = number//10
+# multiplying by 2 and store them in an new array
+index = 0
+for digit in digits:
+    if index%2 == 0:
+        index += 1
+        continue
+    tmp = digit * 2
+    ans += tmp%10 + tmp//10
+    index += 1
+# add all together
+index = 2
+for digit in digits:
+    if (index%2) != 0:
+        index += 1
+        continue
+    ans = ans + digit
+    index += 1
+
 # first two digits
-ftd = cardNum[0]*10 + cardNum[1]
+ftd = digits[0]*10 + digits[1]
 if ans%10 == 0:
-    if cardNum[0] == 4 and count >= 13 and count <= 16:
+    if digits[0] == 4 and count_digits >= 13 and count_digits <= 16:
         print("VISA")
-    elif (51 <= ftd <= 54) and count == 16:
+    elif 51 <= ftd <= 54 and count_digits == 16:
         print("MASTERCARD")
-    elif (ftd == 34 or ftd == 37) and count == 15:
+    elif (ftd == 34 or ftd == 37) and count_digits == 15:
         print("AMEX")
 else:
     print("INVALID")
--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -1,22 +1,66 @@
 from sys import argv, exit
 import csv
-if len(argv) != 3:
-    print("Usage: python dna.py data.csv sequence.txt")
-    exit(1)
+import re
 
-# read database.csv
-with open(argv[1], newline='') as file:
-    if file.mode != 'r':
-        print(f"database {argv[1]} can not be read")
+def main():
+    if len(argv) != 3:
+        print("Usage: python dna.py data.csv sequence.txt")
         exit(1)
-    database = csv.reader(file)
-    for row in database:
-        print(row)
-# read DNA sequence
-with open(argv[2], "r") as file:
-    if file.mode != 'r':
-        print(f"database {argv[2]} can not be read")
-        exit(1)
-    sequ = file.read()
 
-print(sequ)
+    # read DNA sequence
+    with open(argv[2], "r") as file:
+        if file.mode != 'r':
+            print(f"database {argv[2]} can not be read")
+            exit(1)
+        sequence = file.read()
+
+    # read database.csv
+    with open(argv[1], newline='') as file:
+        if file.mode != 'r':
+            print(f"database {argv[1]} can not be read")
+            exit(1)
+        # get the heading of the csv file in order to obtain STRs
+        csv_reader = csv.reader(file)
+        headings = next(csv_reader)
+        # dictionary to store STRs match result of DNA-sequence
+        STR_counter = {}
+        for STR in headings[1::]:
+            # entry result accounting to the STR keys
+            STR_counter[STR] = countSTR(STR, sequence)
+    # read csv file as a dictionary
+    with open(argv[1], newline='') as file:
+        database = csv.DictReader(file)
+        for row in database:
+            count = 0
+            for STR in STR_counter:
+                # print("row in database ", row[STR], "STR in STR_counter", STR_counter[STR])
+                if int(row[STR]) == int(STR_counter[STR]):
+                    count += 1
+            if count == len(STR_counter):
+                print(row['name'])
+                exit(0)
+        else:
+            print("No match")
+
+# find non-overlapping occurrences of STR in DNA-sequence
+def countSTR(STR, sequence):
+    count = 0
+    maxcount = 0
+    # in order to match repeat STR. for example: "('AATG')+" as pattern
+    # into re.compile() to match repeat STR
+    # rewrite STR to "(STR)+"
+    STR = "(" + STR + ")+"
+    pattern = re.compile(r'STR')
+    # matches should be a iterator object
+    matches = pattern.finditer(sequence)
+    # go throgh every repeat and find the longest one
+    # by match.end() - match.start()
+    for match in matches:
+        count = match.end() - match.start()
+        if count > maxcount:
+            maxcount = count
+    # return repeat times of the longest repeat
+    return maxcount/len(STR)
+
+main()
+
